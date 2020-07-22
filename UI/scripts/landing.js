fd3872a7c18c26f507371f27a57c2239a89a7fec
Elvis Iraguha <34744026+elvisiraguha@users.noreply.github.com>
2020-07-22 10:26:45 +0200

    Frontend Sign In Page (#3)
    * feature(frontend-page): build signin page
    
    - create signin form
    - allow signin and signout on blogs page
    
    * fix(frontend-page): fix edit blog modal
    
    * fix the sign in & sign out link on the landing page

--- UI/scripts/landing.js
+++ UI/scripts/landing.js
@@ -1,7 +1,39 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav ul");
+// check for signed in user
+const isSignedIn = localStorage.getItem("signedIn");
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
-  burger.classList.toggle("toggle");
-});
+// handle responsiveness
+const responsive = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav ul");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+};
+
+const handleLogout = () => {
+  localStorage.setItem("signedIn", false);
+  window.location.reload();
+};
+
+// display edits and write when user is signed in
+const isAuthor = (authorized) => {
+  // display either signout or signin nav link if use is singned in
+  const signInOut = document.querySelector(".sign-in-out-link");
+  if (authorized) {
+    const button = document.createElement("button");
+    button.setAttribute("class", "signout-btn");
+    button.textContent = "Sign Out";
+    button.addEventListener("click", handleLogout);
+    signInOut.appendChild(button);
+  } else {
+    const a = document.createElement("a");
+    a.setAttribute("href", "./UI/pages/signin.html");
+    a.textContent = "Sign In";
+    signInOut.appendChild(a);
+  }
+};
+
+responsive();
+isAuthor(isSignedIn === "true" ? true : false);
