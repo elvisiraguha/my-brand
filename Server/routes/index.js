0ae106def4c03b8f675b7769af59575e6f3e6863
elvisiraguha <iraguhaelvis@gmail.com>
2020-08-27 10:27:07 +0200

    add cors

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,14 +1,19 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 import articles from "./articles.router.js";
 import landing from "./profile.router.js";
 import auth from "./auth.router.js";
 import queries from "./queries.router.js";
 import Response from "../helpers/responses.js";
+import swaggerJSON from "../../swagger.json";
 import "../seeds/user.seed.js";
 import "../seeds/profileInfo.seed.js";
 
 const router = express.Router();
 
+router.use(cors())
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 router.use("/api/articles", articles);
 router.use("/api/profile", landing);
 router.use("/api/auth", auth);
